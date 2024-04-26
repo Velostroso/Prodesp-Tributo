da6dbc8a22c1ba24245b4e760d0f42d9ef77aef5
Matheus Veloso Lino <114839388+Velostroso@users.noreply.github.com>
2024-04-26 11:45:01 -0300

    Add files via upload

---
+++ Prodesp-Tributo/src/main/java/br/com/itau/prodesp/Tributo.java
@@ -0,0 +1,27 @@
+package br.com.itau.prodesp;
+
+public class Tributo {
+    private String tipo;
+    private double valor;
+
+    public Tributo(String tipo, double valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
